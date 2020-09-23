2c58d41f19f5a31fb2795ae22e3a7bf24f8d4917
bennesp <bennesp@users.noreply.github.com>
2020-09-23 14:54:07 +0200

    add gh-issue (#1)
    * add gh-issue

---
+++ gh-issue.10m.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+
+PATH=$PATH:/usr/local/bin/
+LOGIN=bennesp
+
+query=$(cat <<-END
+{
+  search(query: "type:pr state:open review-requested:$LOGIN", type: ISSUE, first: 100) {
+    issueCount
+    edges {
+      node {
+        ... on PullRequest {
+          repository {
+            nameWithOwner
+          }
+          author {
+            login
+          }
+          createdAt
+          number
+          url
+          title
+          labels(first:100) {
+            nodes {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+}
+END
+)
+
+response=$(gh api graphql --paginate -f query="$query")
+
+issueCount=$(echo $response | jq -r '.data.search.issueCount')
+titles=$(echo $response | jq -r '.data.search.edges[].node | (.title + " | href=" + .url)')
+
+echo $issueCount
+echo "---"
+echo "$titles"
+
