1551ee1537ed322b9e444a9d44bf3dcadaf4b245
Benedetto <bennesp@users.noreply.github.com>
2020-09-23 14:27:19 +0200

    add gh-pr

---
+++ gh-pr.10m.sh
@@ -0,0 +1,45 @@
+#!/bin/zsh
+
+# Configure LOGIN with your username. Will be used as a filter in searching issues
+LOGIN=bennesp
+
+PATH=$PATH:/usr/local/bin/
+query=$(cat <<-END
+{
+  search(query: "state:open assignee:$LOGIN", type: ISSUE, first: 100) {
+    issueCount
+    edges {
+      node {
+        ... on Issue {
+          repository {
+            nameWithOwner
+          }
+          author {
+            login
+          }
+          createdAt
+          number
+          url
+          title
+          labels(first:100) {
+            nodes {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+}
+END
+)
+
+response=$(gh api graphql --paginate -f query="$query")
+
+issueCount=$(echo $response | jq -r '.data.search.issueCount')
+titles=$(echo $response | jq -r '.data.search.edges[].node | (.title + " | href=" + .url)')
+
+echo $issueCount
+echo "---"
+echo "$titles"
+
