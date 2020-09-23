f825d9d196d29911e282559cc795f6987684366d
Benedetto <bennesp@users.noreply.github.com>
2020-09-23 15:11:00 +0200

    add images

--- gh-issue.10m.sh
+++ gh-issue.10m.sh
@@ -38,7 +38,9 @@ response=$(gh api graphql --paginate -f query="$query")
 issueCount=$(echo $response | jq -r '.data.search.issueCount')
 titles=$(echo $response | jq -r '.data.search.edges[].node | (.title + " | href=" + .url)')
 
-echo $issueCount
+IMAGE="iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAQAAABLCVATAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZcwAAFiUAABYlAUlSJPAAAAAHdElNRQfkCRcNByjz81MNAAABvUlEQVRIx92WvUoDQRSFv4iktHRRg636ACms/AMTQQOW+iARtNBW00qeQbFLGhUiCDHaBEVJE9uoxDKloNdiJuzs7mwyK1Y5gcCee+7JnbPh7sLIIjWknsZjEviky9ffjHIsss6iwTS45oGrJHNuUkFiPhU2XW0OjbY3apxySo03gy0nsWmzTSZQybBNW1dvXG3OYtJLce4yVd9mZeCPrWrVYXzESjA/9PgLWhkTu7pTxRA7jfDDVIjd03fQgjyCUI/wywjCUiSrOwQh3yfGjJMDlIYeS0E4MboCRmsAPDoawZPRZSCNILxbGuxHA/hAENLBiTwAXp3n8dWezaiVyKhlM1L4SWT0DYCoi3FNdgGsf8VnLoAXS0WpP4OkCruTaKKOGbaPBoIwE5FPkCPHRITPIAiN/qWf0TUA2UjDAZdcsh/hs0ZXwOgegONIg2d8+0hp5b3tzFW9iYLYpUePnRB7hiBU7eFt6eWwHEkjE2JWBq8RONKCOQZhXquOBon6VsVYRdHFxrSqU2A2UJmlQN3VxrRS++CWMmVueTdYJxsVe/U/HpAKG5RoBiyalPzVGobLS4QHdIe9RIwwfgEBvdRQl7wQtQAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyMC0wOS0yM1QxMzowNzo0MCswMDowMGl4RyEAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjAtMDktMjNUMTM6MDc6NDArMDA6MDAYJf+dAAAAAElFTkSuQmCC"
+
+echo "$issueCount | image=$IMAGE"
 echo "---"
 echo "$titles"
 
--- gh-pr.10m.sh
+++ gh-pr.10m.sh
@@ -1,13 +1,8 @@
 #!/bin/zsh
 
-# Requirements:
-# - gh: brew install gh
-# - jq: brew install jq
-
-# Configure LOGIN with your username. Will be used as a filter in searching issues
+PATH=$PATH:/usr/local/bin/
 LOGIN=bennesp
 
-PATH=$PATH:/usr/local/bin/
 query=$(cat <<-END
 {
   search(query: "state:open assignee:$LOGIN", type: ISSUE, first: 100) {
@@ -43,7 +38,9 @@ response=$(gh api graphql --paginate -f query="$query")
 issueCount=$(echo $response | jq -r '.data.search.issueCount')
 titles=$(echo $response | jq -r '.data.search.edges[].node | (.title + " | href=" + .url)')
 
-echo $issueCount
+IMAGE="iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAQAAABLCVATAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZcwAAFiUAABYlAUlSJPAAAAAHdElNRQfkCRcNCTfgeHN2AAABdklEQVRIx+2Uv0sCYRjHP0aLKLZETREODRkITUaTW4GLU1PQ0ubW0OpNRi0OUbg1JwQRqPUPVDY0G4RSk0Vutt7T4Nt5r/dDjwob/N50z+fe7z33vfd5YaJ/qHTQBduUuOKAGa2aR4LZ5BF1PbIwUA2gVT4tI+F4wDyAdhGENHNcIHQJaz2amDS5dF86rd1FAXjinVcgwiINViwaAuLEMZka1tE6gtDiDEG4V9Wy6sjAoIYgnA8zinJny8iw6mUroxC3CC+j5LSHIFzbPunbqicDwRwtcr2bvpWv0dDYLG3549GNhmiMRh47/M87Cjjr3kah3zL6sdboqAEpufKSoh1SfjYx26QJBQcv2KhJzNvIQBByZKha51FfYboIVTLkEIS8d0a9f3VChToQIaHRBBHggQqnzh70g60FwD5vavabLnSZHeZdqKYkbVsKhw5+ZKNtkn5xZ2moB4uuvKhog6wOnBtvlg2WqFH3eFWKTZ654YOJxqUvLBCba5CFqo8AAAAldEVYdGRhdGU6Y3JlYXRlADIwMjAtMDktMjNUMTM6MDk6NTUrMDA6MDDpI1irAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIwLTA5LTIzVDEzOjA5OjU1KzAwOjAwmH7gFwAAAABJRU5ErkJggg=="
+
+echo "$issueCount | image=$IMAGE"
 echo "---"
 echo "$titles"
 
